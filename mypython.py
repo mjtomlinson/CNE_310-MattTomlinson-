5b5235f68804b9129be4d2cd2152d6716aa0fc5a
mjtomlinson <mjtomlinson@student.rtc.edu>
2021-05-06 20:28:37 -0700

    Add files via upload

---
+++ mypython.py
@@ -0,0 +1,10 @@
+# Matt_Tomlinson's_Pyhton_commit
+def isDivisible(x, y):
+    if x % y == 0:
+        result = True
+    else:
+        result = False
+
+    return result
+
+print(isDivisible(10, 5))
